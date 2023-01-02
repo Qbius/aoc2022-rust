234ba3fb858757f22a59d4d5fb182c588bdca94a
qbius <theqbius@gmail.com>
2023-01-02 23:15:43 +0100

    day 2

--- src/aoc/day2.rs
+++ src/aoc/day2.rs
@@ -1,8 +1,18 @@
-
-pub fn first(_input: &String) -> Option<String> {
-    None
+fn parse(input: &String) -> Vec<(i32, i32)> {
+    input
+        .split('\n').map(|line| line.split(' ').map(|s| s.chars().nth(0).unwrap()).collect::<Vec<char>>())
+        .map(|vec| ((vec[0] as i32) - ('A' as i32), (vec[1] as i32) - ('X' as i32)))
+        .collect::<Vec<(i32, i32)>>()
 }
 
-pub fn second(_input: &String) -> Option<String> {
-    None
+pub fn first(input: &String) -> Option<String> {
+    let games = parse(input);
+    let sum = games.iter().map(|(op, me)| (me + 1) + (((me - op) + 1).rem_euclid(3) * 3)).sum::<i32>();
+    Some(sum.to_string())
+}
+
+pub fn second(input: &String) -> Option<String> {
+    let games = parse(input);
+    let sum = games.iter().map(|(op, me)| ((op + me - 1).rem_euclid(3) + 1) + me * 3).sum::<i32>();
+    Some(sum.to_string())  
 }
